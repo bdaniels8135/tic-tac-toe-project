44ae57b918177166863a75a207a1a7c8c7ee5e78
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-10-21 19:25:23 -0400

    Create click listeners refacter gameboard creation

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function createPlayer(type, mark) {
     return Player;
 };
 
-function createGameBoard() {
+function createGameboard() {
     const Gameboard = (function() {
         const _INITIAL_CELL_CONTENTS = [...Array(3)].map(e => Array(3).fill(null));
     
@@ -68,9 +68,8 @@ function createGameBoard() {
     return Gameboard;
 };
 
-function createGame(playerOne, playerTwo) {
+function createGame(playerOne, playerTwo, gameboard) {
     const Game = (function() {
-        const _GAMEBOARD = createGameBoard();
         let _isGameOver = false;
         let _activePlayer = playerOne;
 
@@ -81,11 +80,11 @@ function createGame(playerOne, playerTwo) {
         function _getPossibleWinContents() {
             let results = [];
             for (let i = 0; i <= 2; i++) {
-                results.push(_GAMEBOARD.getRowContent(i));
-                results.push(_GAMEBOARD.getColumnContent(i));
+                results.push(gameboard.getRowContent(i));
+                results.push(gameboard.getColumnContent(i));
             };
-            results.push(_GAMEBOARD.getMainDiagonalContent());
-            results.push(_GAMEBOARD.getMinorDiagonalContent());
+            results.push(gameboard.getMainDiagonalContent());
+            results.push(gameboard.getMinorDiagonalContent());
             return results;
         };
 
@@ -105,10 +104,10 @@ function createGame(playerOne, playerTwo) {
         };
         
         function playTurn(row, column) {
-            if (!!_GAMEBOARD.getCellContent(row, column)) {
+            if (!!gameboard.getCellContent(row, column)) {
                 return; // Some kind of error message?
             };
-            _GAMEBOARD.setCellContent(_activePlayer.getMark(), row, column);
+            gameboard.setCellContent(_activePlayer.getMark(), row, column);
             _checkGameOver();
             _switchActivePlayer();
         };
@@ -132,7 +131,7 @@ function createGame(playerOne, playerTwo) {
 }
 
 
-function createDisplayController() {
+function createDisplayController(gameboard) {
     const DisplayController = (function() {
         const _UI = {
             X_BTN: document.getElementById('x-mark'),
@@ -143,15 +142,35 @@ function createDisplayController() {
             GAME_CONTAINER: document.getElementById('game-container'), 
         };
         
-        _UI.X_BTN.addEventListener('click', event => console.log(event.target.id));
-        _UI.O_BTN.addEventListener('click', event => console.log(event.target.id));
-        _UI.DUMB_AI_BTN.addEventListener('click', event => console.log(event.currentTarget.id));
-        _UI.MASTER_AI_BTN.addEventListener('click', event => console.log(event.currentTarget.id));
-        _UI.HUMAN_BTN.addEventListener('click', event => console.log(event.currentTarget.id));
+        let _selectedMark = null;
+        let _selectedOpponent = null;
 
-        function _gameGridClickListener(event) {
-            
+        function _resolveMarkBtnClick(event) {
+            _UI.X_BTN.classList.remove('selected');
+            _UI.O_BTN.classList.remove('selected');
+            event.target.classList.add('selected');
+
+            console.log(event.target.id)
         };
+
+        function _resolveOpponentBtnClick(event) {
+            _UI.DUMB_AI_BTN.classList.remove('selected');
+            _UI.MASTER_AI_BTN.classList.remove('selected');
+            _UI.HUMAN_BTN.classList.remove('selected');
+            event.currentTarget.classList.add('selected');
+
+            console.log(event.currentTarget.id)
+        };
+
+        function _resolveGameCellClick(event) {
+            console.log(event.target.id)
+        };
+
+        _UI.X_BTN.addEventListener('click', _resolveMarkBtnClick);
+        _UI.O_BTN.addEventListener('click', _resolveMarkBtnClick);
+        _UI.DUMB_AI_BTN.addEventListener('click', _resolveOpponentBtnClick);
+        _UI.MASTER_AI_BTN.addEventListener('click', _resolveOpponentBtnClick);
+        _UI.HUMAN_BTN.addEventListener('click', _resolveOpponentBtnClick);
 
         function renderGameGrid() {
             let gameRow;
@@ -163,21 +182,18 @@ function createDisplayController() {
                 for (let j = 0; j <= 2; j++) {
                     gameCell = document.createElement('div');
                     gameCell.classList.add('game-cell')
-                    gameCell.innerHTML = 'X';
-                    gameCell.addEventListener('click', event => console.log(event));
+                    gameCell.id = `game-cell-${i}${j}`
+                    gameCell.innerHTML = gameboard.getCellContent(i, j);
+                    gameCell.addEventListener('click', _resolveGameCellClick);
                     gameRow.appendChild(gameCell);
                 };
             };
         };
 
-        function deleteGrid() {
+        // Add Win announcement and resign/new game button
 
-        };
-
-        // Add UI content and rendering functions
         return {
             renderGameGrid,
-            _UI,
         };
     })();
 
@@ -185,8 +201,12 @@ function createDisplayController() {
 };
 
 const GameController = (function() { 
-    const _DISPLAY_CONTROLLER = createDisplayController();
-    _DISPLAY_CONTROLLER.renderGameGrid()
+    const _gameboard = createGameboard(); 
+    const _DISPLAY_CONTROLLER = createDisplayController(_gameboard);
+    _DISPLAY_CONTROLLER.renderGameGrid();
+
+
+
     // Have a click trigger this creation?
     // const _playerOne = createPlayer(playerOneType, 'X');
     // const _playerTwo = createPlayer(playerTwoType, 'O');
